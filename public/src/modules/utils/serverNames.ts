498b0e4393d5e8aa1aa9842d34ff0f6599741c04
IvanShport <ivan.shport.98@mail.ru>
2019-04-27 16:10:19 +0300

    [FIX] fix design and deploy ver 2

--- public/src/modules/utils/serverNames.ts
+++ public/src/modules/utils/serverNames.ts
@@ -1,6 +1,6 @@
 const serverNames = {
     avatarBackend: 'http://warscript-images.herokuapp.com',
-    backend: 'http://89.208.198.192/v1/',
+    backend: 'http://89.208.198.192/v1',
     WSbackend: 'ws://localhost:8000',
     // WSbackend: 'ws://89.208.198.192:8001/v1',
 };
