d91a98aee590a15969d5db213171c5a1104bb5a1
GDVFox <gaaveer@gmail.com>
2019-06-02 01:29:28 +0300

    [ADD] Creation error handle

--- public/src/modules/game/game.ts
+++ public/src/modules/game/game.ts
@@ -10,25 +10,25 @@ import Console from '../../components/console/console';
 export const runCode = (game_slug: string, code: string, enemyCode: string) => {
 
     let t
-    if (game_slug === "pong") {
-        try {
+    try {
+        if (game_slug === "pong") {
             const p1 = new Function("me", "enemy", "ball", "game", "memory", code);
             const p2 = new Function("me", "enemy", "ball", "game", "memory", enemyCode);
             const g = new PongGame(250, 500);
             t = new Tester(p1, p2, g, 10000);
-        } catch (err) {
-            Console.createLog(`[CREATION] ERROR: ${err.name}: ${err.message};`, true);
-            return;
+        } else if (game_slug === "2atod") {
+            const p1 = new Function("units", "enemy_units", "dropzone", "enemy_dropzone", "flags",
+                "enemy_flags", "obstacles", "projectiles", "game", code);
+            const p2 = new Function("units", "enemy_units", "dropzone", "enemy_dropzone", "flags",
+                "enemy_flags", "obstacles", "projectiles", "game", code); //TODO: POMENYAI EBACH
+            const g = Atod.stdField();
+            t = new Tester(p1, p2, g, 10000);
+        } else {
+            Alert.alert('poszaluista ne lomai(((', false, 100000)
         }
-    } else if (game_slug === "2atod") {
-        const p1 = new Function("units", "enemy_units", "dropzone", "enemy_dropzone", "flags",
-            "enemy_flags", "obstacles", "projectiles", "game", code);
-        const p2 = new Function("units", "enemy_units", "dropzone", "enemy_dropzone", "flags",
-            "enemy_flags", "obstacles", "projectiles", "game", code); //TODO: POMENYAI EBACH
-        const g = Atod.stdField();
-        t = new Tester(p1, p2, g, 10000);
-    } else {
-        Alert.alert('poszaluista ne lomai(((', false, 100000)
+    } catch (err) {
+        Console.createLog(`[CREATION] ERROR: ${err.name}: ${err.message};`, true);
+        return
     }
 
 
