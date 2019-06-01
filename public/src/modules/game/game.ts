e5179a98182ef50f4456b32184027facec98b08b
GDVFox <gaaveer@gmail.com>
2019-06-02 01:26:08 +0300

    [ADD] Creation error handle

--- public/src/modules/game/game.ts
+++ public/src/modules/game/game.ts
@@ -5,15 +5,21 @@ import Ball from './ping-pong/ball';
 import PongGame from './ping-pong/game';
 import * as Atod from './atod/std_init';
 import Alert from '../../components/alert/alert';
+import Console from '../../components/console/console';
 
 export const runCode = (game_slug: string, code: string, enemyCode: string) => {
 
     let t
     if (game_slug === "pong") {
-        const p1 = new Function("me", "enemy", "ball", "game", "memory", code);
-        const p2 = new Function("me", "enemy", "ball", "game", "memory", enemyCode);
-        const g = new PongGame(250, 500);
-        t = new Tester(p1, p2, g, 10000);
+        try {
+            const p1 = new Function("me", "enemy", "ball", "game", "memory", code);
+            const p2 = new Function("me", "enemy", "ball", "game", "memory", enemyCode);
+            const g = new PongGame(250, 500);
+            t = new Tester(p1, p2, g, 10000);
+        } catch (err) {
+            Console.createLog(`[CREATION] ERROR: ${err.name}: ${err.message};`, true);
+            return;
+        }
     } else if (game_slug === "2atod") {
         const p1 = new Function("units", "enemy_units", "dropzone", "enemy_dropzone", "flags",
             "enemy_flags", "obstacles", "projectiles", "game", code);
