3e098bc867d7ba98f7b47bbda9c48cf05fdc3785
IvanShport <ivan.shport.98@mail.ru>
2019-06-02 02:52:25 +0300

    [FIX] game ver 8

--- public/src/components/games/atod/atod.ts
+++ public/src/components/games/atod/atod.ts
@@ -7,32 +7,35 @@ import Console from '../../../components/console/console';
 import EventBus from '../../../modules/event-bus';
 import {events} from '../../../modules/utils/events';
 import Component from '../../baseComponent/index';
+import SvgGameObject from '../svgGameObject';
 
 class Atod extends BaseGame{
 
     private player1Text: Component;
     private player2Text: Component;
 
-    private dropzoneMe: GameObject;
-    private dropzoneEnemy: GameObject;
+    private atodSvg: Component;
 
-    private player1Tank: GameObject;
-    private player1Healer: GameObject;
-    private player1Sniper: GameObject;
-    private player1DD1: GameObject;
-    private player1DD2: GameObject;
+    private dropzoneMe: SvgGameObject;
+    private dropzoneEnemy: SvgGameObject;
 
-    private player2Tank: GameObject;
-    private player2Healer: GameObject;
-    private player2Sniper: GameObject;
-    private player2DD1: GameObject;
-    private player2DD2: GameObject;
+    private player1Tank: SvgGameObject;
+    private player1Healer: SvgGameObject;
+    private player1Sniper: SvgGameObject;
+    private player1DD1: SvgGameObject;
+    private player1DD2: SvgGameObject;
 
-    private flagsMe: GameObject[];
+    private player2Tank: SvgGameObject;
+    private player2Healer: SvgGameObject;
+    private player2Sniper: SvgGameObject;
+    private player2DD1: SvgGameObject;
+    private player2DD2: SvgGameObject;
 
-    private flagsEnemy: GameObject[];
+    private flagsMe: SvgGameObject[];
 
-    private obstacles: GameObject[];
+    private flagsEnemy: SvgGameObject[];
+
+    private obstacles: SvgGameObject[];
 
     private width: number;
     private height: number;
@@ -42,26 +45,31 @@ class Atod extends BaseGame{
     constructor(el: HTMLElement) {
         super(el);
 
-        this.dropzoneMe = new GameObject(this.el.querySelector('.atod__dropzone_me'));
-        this.dropzoneEnemy = new GameObject(this.el.querySelector('.atod__dropzone_enemy'));
+        this.width = 1000;
+        this.height = 500;
 
-        this.player1Tank = new GameObject(this.el.querySelector('.atod__tank.atod__player1'));
-        this.player1Healer = new GameObject(this.el.querySelector('.atod__healer.atod__player1'));
-        this.player1Sniper = new GameObject(this.el.querySelector('.atod__sniper.atod__player1'));
-        this.player1DD1 = new GameObject(this.el.querySelector('.atod__dd1.atod__player1'));
-        this.player1DD2 = new GameObject(this.el.querySelector('.atod__dd2.atod__player1'));
+        this.atodSvg = new Component(this.el.querySelector('.atod'));
 
-        this.player2Tank = new GameObject(this.el.querySelector('.atod__tank.atod__player2'));
-        this.player2Healer = new GameObject(this.el.querySelector('.atod__healer.atod__player2'));
-        this.player2Sniper = new GameObject(this.el.querySelector('.atod__sniper.atod__player2'));
-        this.player2DD1 = new GameObject(this.el.querySelector('.atod__dd1.atod__player2'));
-        this.player2DD2 = new GameObject(this.el.querySelector('.atod__dd2.atod__player2'));
+        this.dropzoneMe = new SvgGameObject(this.el.querySelector('.atod__dropzone_me'));
+        this.dropzoneEnemy = new SvgGameObject(this.el.querySelector('.atod__dropzone_enemy'));
+
+        this.player1Tank = new SvgGameObject(this.el.querySelector('.atod__tank.atod__player1'));
+        this.player1Healer = new SvgGameObject(this.el.querySelector('.atod__healer.atod__player1'));
+        this.player1Sniper = new SvgGameObject(this.el.querySelector('.atod__sniper.atod__player1'));
+        this.player1DD1 = new SvgGameObject(this.el.querySelector('.atod__dd1.atod__player1'));
+        this.player1DD2 = new SvgGameObject(this.el.querySelector('.atod__dd2.atod__player1'));
+
+        this.player2Tank = new SvgGameObject(this.el.querySelector('.atod__tank.atod__player2'));
+        this.player2Healer = new SvgGameObject(this.el.querySelector('.atod__healer.atod__player2'));
+        this.player2Sniper = new SvgGameObject(this.el.querySelector('.atod__sniper.atod__player2'));
+        this.player2DD1 = new SvgGameObject(this.el.querySelector('.atod__dd1.atod__player2'));
+        this.player2DD2 = new SvgGameObject(this.el.querySelector('.atod__dd2.atod__player2'));
 
         this.flagsMe = Array.from(this.el.querySelectorAll('.atod__flag_me'))
-            .map((flag) => new GameObject(flag as HTMLElement));
+            .map((flag) => new SvgGameObject(flag as HTMLElement));
 
         this.flagsEnemy = Array.from(this.el.querySelectorAll('.atod__flag_enemy'))
-            .map((flag) => new GameObject(flag as HTMLElement));
+            .map((flag) => new SvgGameObject(flag as HTMLElement));
 
         if (this.obstacles && this.obstacles.length > 0) {
             this.obstacles.forEach((obstacle) => {
@@ -82,8 +90,6 @@ class Atod extends BaseGame{
             this.player2Text.setText(Match.user2.username);
         }
 
-        this.width = +this.el.style.width;
-        this.height = +this.el.style.height;
     }
 
     public async init(states: any) {
@@ -97,15 +103,13 @@ class Atod extends BaseGame{
             this.render(states);
         });
 
-        this.dropzoneMe.setWidth(`${states.info.p1_dropzone.radius * this.el.clientHeight}px`);
-        this.dropzoneMe.setHeight(`${states.info.p1_dropzone.radius * this.el.clientHeight}px`);
-        this.dropzoneMe.setX(`${states.info.p1_dropzone.x * this.el.clientWidth}px`);
-        this.dropzoneMe.setY(`${states.info.p1_dropzone.y * this.el.clientHeight}px`);
+        this.dropzoneMe.setRadius(`${states.info.p1_dropzone.radius * this.height}`);
+        this.dropzoneMe.setCX(`${states.info.p1_dropzone.x * this.width}`);
+        this.dropzoneMe.setCY(`${states.info.p1_dropzone.y * this.height}`);
 
-        this.dropzoneEnemy.setWidth(`${states.info.p2_dropzone.radius * this.el.clientHeight}px`);
-        this.dropzoneEnemy.setHeight(`${states.info.p2_dropzone.radius * this.el.clientHeight}px`);
-        this.dropzoneEnemy.setX(`${states.info.p2_dropzone.x * this.el.clientWidth}px`);
-        this.dropzoneEnemy.setY(`${states.info.p2_dropzone.y * this.el.clientHeight}px`);
+        this.dropzoneEnemy.setRadius(`${states.info.p2_dropzone.radius * this.height}`);
+        this.dropzoneEnemy.setCX(`${states.info.p2_dropzone.x * this.width}`);
+        this.dropzoneEnemy.setCY(`${states.info.p2_dropzone.y * this.height}`);
 
         this.flagsMe.forEach((flag) => {
 
@@ -142,91 +146,88 @@ class Atod extends BaseGame{
         state.obstacles.map((obstacle: {[key: string]: any}, i: number) => {
 
             if (!this.obstacles[i]) {
-                const newObst = Component.Create('div', ['atod__obstacle']);
-                this.append(newObst);
-                this.obstacles[i] = new GameObject(newObst.el);
+                const newObst = Component.CreateSVGElem('rect', ['atod__obstacle'], {
+                    width: `${obstacle.width * this.width}`,
+                    height: `${obstacle.height * this.height}`,
+                    x: `${obstacle.x * this.width}`,
+                    y: `${obstacle.y * this.height}`,
+                });
+                this.atodSvg.el.appendChild(newObst);
+                const c = new Component(newObst as Element as HTMLElement);
+                this.obstacles[i] = new SvgGameObject(c);
+            } else {
+
+                this.obstacles[i].setX(`${obstacle.x * this.width}`);
+                this.obstacles[i].setY(`${obstacle.y * this.height}`);
+                this.obstacles[i].setWidth(`${obstacle.width * this.width}`);
+                this.obstacles[i].setHeight(`${obstacle.height * this.height}`);
             }
-            this.obstacles[i].setX(`${obstacle.x * this.el.clientWidth}px`);
-            this.obstacles[i].setY(`${obstacle.y * this.el.clientHeight}px`);
-            this.obstacles[i].setWidth(`${obstacle.width * this.el.clientWidth}px`);
-            this.obstacles[i].setHeight(`${obstacle.height * this.el.clientHeight}px`);
         });
 
         state.p1_units.map((unit: {[key: string]: any}) => {
 
             if (unit.unit_type === 'tank') {
-                this.player1Tank.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player1Tank.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player1Tank.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player1Tank.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player1Tank.setCX(`${unit.x * this.width}`);
+                this.player1Tank.setCY(`${unit.y * this.height}`);
+                this.player1Tank.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'sniper') {
 
-                this.player1Sniper.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player1Sniper.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player1Sniper.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player1Sniper.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player1Sniper.setCX(`${unit.x * this.width}`);
+                this.player1Sniper.setCY(`${unit.y * this.height}`);
+                this.player1Sniper.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'medic') {
 
-                this.player1Healer.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player1Healer.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player1Healer.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player1Healer.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player1Healer.setCX(`${unit.x * this.width}`);
+                this.player1Healer.setCY(`${unit.y * this.height}`);
+                this.player1Healer.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'soldier1') {
 
-                this.player1DD1.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player1DD1.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player1DD1.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player1DD1.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player1DD1.setCX(`${unit.x * this.width}`);
+                this.player1DD1.setCY(`${unit.y * this.height}`);
+                this.player1DD1.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'soldier2') {
 
-                this.player1DD2.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player1DD2.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player1DD2.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player1DD2.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player1DD2.setCX(`${unit.x * this.width}`);
+                this.player1DD2.setCY(`${unit.y * this.height}`);
+                this.player1DD2.setRadius(`${unit.radius * this.height}`);
             }
 
         });
 
-
         state.p2_units.map((unit: {[key: string]: any}) => {
 
             if (unit.unit_type === 'tank') {
-                this.player2Tank.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player2Tank.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player2Tank.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player2Tank.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player2Tank.setCX(`${unit.x * this.width}`);
+                this.player2Tank.setCY(`${unit.y * this.height}`);
+                this.player2Tank.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'sniper') {
 
-                this.player2Sniper.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player2Sniper.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player2Sniper.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player2Sniper.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player2Sniper.setCX(`${unit.x * this.width}`);
+                this.player2Sniper.setCY(`${unit.y * this.height}`);
+                this.player2Sniper.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'medic') {
 
-                this.player2Healer.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player2Healer.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player2Healer.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player2Healer.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player2Healer.setCX(`${unit.x * this.width}`);
+                this.player2Healer.setCY(`${unit.y * this.height}`);
+                this.player2Healer.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'soldier1') {
 
-                this.player2DD1.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player2DD1.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player2DD1.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player2DD1.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player2DD1.setCX(`${unit.x * this.width}`);
+                this.player2DD1.setCY(`${unit.y * this.height}`);
+                this.player2DD1.setRadius(`${unit.radius * this.height}`);
 
             } else if (unit.unit_type === 'soldier2') {
 
-                this.player2DD2.setX(`${unit.x * this.el.clientWidth}px`);
-                this.player2DD2.setY(`${unit.y * this.el.clientHeight}px`);
-                this.player2DD2.setWidth(`${unit.radius * 2 * this.el.clientHeight}px`);
-                this.player2DD2.setHeight(`${unit.radius * 2 * this.el.clientHeight}px`);
+                this.player2DD2.setCX(`${unit.x * this.width}`);
+                this.player2DD2.setCY(`${unit.y * this.height}`);
+                this.player2DD2.setRadius(`${unit.radius * this.height}`);
             }
 
         });
@@ -235,8 +236,8 @@ class Atod extends BaseGame{
 
             if (state.p1_flags[i]) {
 
-                this.flagsMe[i].setX(`${state.p1_flags[i].x * this.el.clientWidth}px`);
-                this.flagsMe[i].setY(`${state.p1_flags[i].y * this.el.clientHeight}px`);
+                this.flagsMe[i].setX(`${state.p1_flags[i].x * this.width}`);
+                this.flagsMe[i].setY(`${state.p1_flags[i].y * this.height}`);
 
             } else {
 
@@ -249,8 +250,8 @@ class Atod extends BaseGame{
 
             if (state.p2_flags[i]) {
 
-                this.flagsEnemy[i].setX(`${state.p2_flags[i].x * this.el.clientWidth}px`);
-                this.flagsEnemy[i].setY(`${state.p2_flags[i].y * this.el.clientHeight}px`);
+                this.flagsEnemy[i].setX(`${state.p2_flags[i].x * this.width}`);
+                this.flagsEnemy[i].setY(`${state.p2_flags[i].y * this.height}`);
 
             } else {
 
